15854ad5e766540f12891ca2907ef7dad8f1aae4
Geordon Worley <vadixidav@gmail.com>
2020-02-26 12:15:05 -0500

    break compatibility; add LoadQuery trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use diesel::{
     backend::UsesAnsiSavepointSyntax,
     insertable::CanInsertInSingleQuery,
     query_builder::{InsertStatement, QueryFragment},
-    query_dsl::methods::{ExecuteDsl, LoadQuery},
+    query_dsl::methods::ExecuteDsl,
     r2d2::{ConnectionManager, PooledConnection},
     Insertable, RunQueryDsl,
 };
@@ -41,6 +41,23 @@ where
 {
 }
 
+pub trait LoadQuery<Item, BaseConnection>:
+    diesel::query_dsl::methods::LoadQuery<Connection<BaseConnection>, Item>
+where
+    BaseConnection: diesel::connection::Connection + 'static,
+{
+}
+
+impl<T, Item, BaseConnection> LoadQuery<Item, BaseConnection> for T
+where
+    T: diesel::query_dsl::methods::LoadQuery<Connection<BaseConnection>, Item>,
+    BaseConnection: diesel::connection::Connection<
+            TransactionManager = diesel::connection::AnsiTransactionManager,
+        > + 'static,
+    BaseConnection::Backend: UsesAnsiSavepointSyntax,
+{
+}
+
 pub struct Db<BaseConnection: diesel::connection::Connection + 'static> {
     pool: Pool<BaseConnection>,
 }
@@ -66,8 +83,8 @@ where
 impl<BaseConnection> Db<BaseConnection>
 where
     BaseConnection: diesel::connection::Connection<
-            TransactionManager = diesel::connection::AnsiTransactionManager,
-        > + 'static,
+        TransactionManager = diesel::connection::AnsiTransactionManager,
+    >,
     BaseConnection::Backend: UsesAnsiSavepointSyntax,
 {
     pub fn cud<C>(&self, cud: C) -> Result<(), Error>
@@ -78,18 +95,24 @@ where
         cud.execute(self)
     }
 
-    pub fn cud_query<Q>(&self, query: Q) -> Result<(), Error>
-    where
-        Q: CudQuery<BaseConnection>,
-    {
+    pub fn cud_query(&self, query: impl CudQuery<BaseConnection>) -> Result<(), Error> {
         Ok(query.execute(&self.conn()?).map(|_| ())?)
     }
+}
 
+impl<BaseConnection> Db<BaseConnection>
+where
+    BaseConnection: diesel::connection::Connection,
+{
     pub fn load<L>(&self, load: L) -> Result<Vec<L::Item>, Error>
     where
         L: Load<BaseConnection>,
     {
         load.load(self)
+    }
+
+    pub fn load_query<T>(&self, query: impl LoadQuery<T, BaseConnection>) -> Result<Vec<T>, Error> {
+        Ok(query.load::<T>(&self.conn()?)?)
     }
 }
 
@@ -142,10 +165,10 @@ where
     BaseConnection: diesel::connection::Connection + 'static,
 {
     type Item;
-    type Query: LoadQuery<Connection<BaseConnection>, Self::Item>;
+    type Query: LoadQuery<Self::Item, BaseConnection>;
 
     fn load(self, db: &Db<BaseConnection>) -> Result<Vec<Self::Item>, Error> {
-        Ok(self.query().load::<Self::Item>(&db.conn()?)?)
+        db.load_query(self.query())
     }
 
     fn query(self) -> Self::Query;
